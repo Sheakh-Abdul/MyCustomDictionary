bf97c700991a13ee33cb4589ee81d5c5b5fdb852
Abdul Samad <abdulsheakh043@gmail.com>
2025-09-07 15:26:59 +0530

    Dictinary File with TestCase File.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Diagnostics;
+﻿using MyDictionaryProject;
+using System.Diagnostics;
 
 
 namespace ConsoleApp2
@@ -6,29 +7,14 @@ namespace ConsoleApp2
     class Program
     {
         static void Main()
-        {
-            const int itemCount = 100_000;
-
-            var customDict = new MyCustomDictionary<string, int>();
-            var systemDict = new Dictionary<string, int>();
-
-            var sw = Stopwatch.StartNew();
-            for (int i = 0; i < itemCount; i++)
-            {
-                customDict.Add(i.ToString(), i);
-            }
-            sw.Stop();
-            Console.WriteLine($"MyCustomDictionary Add: {sw.ElapsedMilliseconds} ms");
-
-            sw.Restart();
-            for (int i = 0; i < itemCount; i++)
-            {
-                systemDict.Add(i.ToString(), i);
-            }
-            sw.Stop();
-            Console.WriteLine($".NET Dictionary Add: {sw.ElapsedMilliseconds} ms");
-
-            Console.WriteLine("Benchmark complete.");
+        { 
+            LongestSum sum = new LongestSum();
+            Console.WriteLine(sum.LongestSubStringWithRepeat("AABABBA", 1));
+            //var res = sum.FindAnagrams("cbaebabacd", "abc");
+            //foreach (var item in res)
+            //{
+            //    Console.WriteLine(item);
+            //}
         }
     }
 }
